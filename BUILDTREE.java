2ff9508a6960f1d7fc8a6ccdbd8df5599830575b
Vaibhavi Sawant <96656695+VaibhaviSawant101@users.noreply.github.com>
2023-09-18 21:54:05 +0530

    Add files via upload

---
+++ BUILDTREE.java
@@ -0,0 +1,39 @@
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data)
+    {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BinarySearchTree {
+    static int idx = -1;
+
+    public static Node buildTree(int[] arr) {
+        idx++;
+        if (arr[idx] == -1) {
+            return null;
+        }
+
+        Node newnode = new Node(arr[idx]);
+        newnode.left = buildTree(arr);
+        newnode.right = buildTree(arr);
+
+        return newnode;
+    }
+}
+
+public class BUILDTREE {
+    public static void main(String[] args) {
+        BinarySearchTree b1 = new BinarySearchTree();
+        int[] arr = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        Node root = b1.buildTree(arr);
+        System.out.println(root.data);
+    }
+}
